d31ade17fadc23e21574cbc5410b1b239fed0236
Robert Egeland <robegeland@yahoo.com>
2022-08-04 20:00:48 -0500

    commit tree 74cf53b79703fb2192ac49a11f69856a519a7540

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validate :fiction_or_nonfiction
+    validate :clickbait
+
+    def fiction_or_nonfiction
+        unless self.category == "Fiction" || self.category == "Non-Fiction"
+            errors.add(:category, "is not included in the list")
+        end
+    end
+
+    def clickbait
+        if title == "True Facts"
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
